f98fa7322e783ca11446c1ea4deda2b84de81e9e
Vishal Sharma <vishalsharma34258@gmail.com>
2023-08-22 19:55:34 +0530

    Created the data for semesters in the backend and also implemented their linking with the respective branches

--- backend/routes/semester.js
+++ backend/routes/semester.js
@@ -4,22 +4,6 @@ const Semester = require("../models/Semester");
 
 const router = express.Router();
 
-// const branches = [
-//   "Chemical Engineering",
-//   "Mechanical Engineering",
-//   "Civil Engineering",
-//   "Computer Science & Engg",
-//   "Electrical Engineering",
-//   "E&C Engineering",
-//   "Metallurgical Engineering",
-//   "Information technology",
-//   "Mathematics",
-//   "Physics",
-//   "Chemistry",
-//   "HSS&M",
-//   "Training & Placements",
-// ];
-
 router.get("/", async (req, res) => {
   const semesters = await Semester.find({});
   return res.status(200).json({ data: semesters });
